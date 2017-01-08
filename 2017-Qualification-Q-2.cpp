ab74c304203c312b307de601d50eb3eb22c0e1f0
Nitin Kumar Maharana <nitinkumar.maharana@accelerite.com>
2017-01-08 18:45:25 +0530

    Lazy Loading - Facebook Hacker Cup 2017 Qualification Round
    https://www.facebook.com/hackercup/problem/169401886867367/

---
+++ 2017-Qualification-Q-2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int getCounts(int *w, int n) {
+	sort(w, w+n);
+
+	int l, r, count, sum;
+
+	l = 0;
+	r = n-1;
+	count = 0;
+
+	while(l <= r) {
+		sum = w[r];
+		r--;
+
+		if(sum >= 50) {
+			count++;
+			continue;
+		}
+
+		while(l <= r && sum < 50) {
+			l++;
+			sum += w[r+1];
+		}
+
+		if(sum >= 50)
+			count++;
+	}
+
+	return count;
+}
+
+int main(void) {
+	int t, n, j, *w;
+
+	cin >> t;
+	j = 1;
+
+	while(j <= t) {
+		cin >> n;
+
+		w = new int[n];
+
+		for(int i = 0; i < n; i++)
+			cin >> w[i];
+
+		cout << "Case #" << j << ": " << getCounts(w, n) << endl;
+
+		delete[] w;
+		j++;
+	}
+
+	return 0;
+}
